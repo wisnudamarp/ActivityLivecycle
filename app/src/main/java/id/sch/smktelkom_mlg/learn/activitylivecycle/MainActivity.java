2233610cdd0b9db660ac42bf20adc7099f1f865a
Wisnu <wisnudamar001@gmail.com>
2016-11-05 14:55:19 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.learn.activitylivecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LivecycleTag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+}
